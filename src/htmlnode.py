0ae83708ba32750749cc52bc1f783a7ab211c0c5
Contemplative-Vagabond <dg.contemplativevagabond@gmail.com>
2025-03-15 19:46:57 +0100

    add LeafNode class to htmlnode.py and append test_leafnode.py to .gitignore

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,19 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, [], props)
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None:
+            return str(self.value)
+        if self.props is not None:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
